cb9461d9adc94b941c8827b95d9fdc0ca9ae1d82
Kishore Koppula <kkishore906@gmail.com>
2025-02-26 10:24:19 +1100

    db update

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected..");
   });
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect(`${process.env.MONGODB_URL}/lms-mern-app`);
 };
 
 export default connectDB;
